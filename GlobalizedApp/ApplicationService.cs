ff707c1bcbbd04642fa9579efc496c0c6092c5b9
murilobeltrame <murilobeltrame@gmail.com>
2024-05-07 01:05:37 -0300

    ...

---
+++ GlobalizedApp/ApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+
+namespace GlobalizedApp;
+
+internal class ApplicationService(ILogger<ApplicationService> logger, IStringLocalizer<ApplicationService> localizer) : IHostedService
+{
+    private readonly IStringLocalizer _localizer = localizer;
+    private readonly ILogger<ApplicationService> _logger = logger;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("{msg}", _localizer["STARTING_MESSAGE"]);
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
---
+++ GlobalizedApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+using GlobalizedApp;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args);
+builder.ConfigureServices(services =>
+{
+    var cultureInfo = new CultureInfo("pt", false);
+    CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+    CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+    CultureInfo.CurrentCulture = cultureInfo;
+
+    services.AddLocalization(o => o.ResourcesPath = "Resources");
+    services.AddHostedService<ApplicationService>();
+});
+
+var host = builder.Build();
+host.Run();
